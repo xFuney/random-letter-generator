e2056ebac46dfcaccd66493c511944a59efc34f5
xFuney (Funey) <56867550+xFuney@users.noreply.github.com>
2020-05-13 12:55:59 +0100

    Added code, program now works. Minor UI fixes.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,19 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.RandomVal = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.RandomVal = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
-            // 
-            // RandomVal
-            // 
-            this.RandomVal.AutoSize = true;
-            this.RandomVal.Font = new System.Drawing.Font("Microsoft Sans Serif", 99.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.RandomVal.Location = new System.Drawing.Point(81, 9);
-            this.RandomVal.Name = "RandomVal";
-            this.RandomVal.Size = new System.Drawing.Size(155, 152);
-            this.RandomVal.TabIndex = 0;
-            this.RandomVal.Text = "A";
             // 
             // button1
             // 
@@ -51,14 +41,26 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "Spin the Dice";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // RandomVal
+            // 
+            this.RandomVal.Font = new System.Drawing.Font("Microsoft Sans Serif", 90F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RandomVal.Location = new System.Drawing.Point(12, 13);
+            this.RandomVal.Multiline = true;
+            this.RandomVal.Name = "RandomVal";
+            this.RandomVal.ReadOnly = true;
+            this.RandomVal.Size = new System.Drawing.Size(280, 133);
+            this.RandomVal.TabIndex = 2;
+            this.RandomVal.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(304, 231);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.RandomVal);
+            this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "Form1";
@@ -69,9 +71,8 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label RandomVal;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox RandomVal;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,30 @@ namespace RandomLetterGen
 {
     public partial class Form1 : Form
     {
+        private string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        // Random Number Function
+        public int RandomNumber(int min, int max)
+        {
+            Random random = new Random(); // Generates a variable with datatype Random.
+            return random.Next(min, max); // Returns the random number.
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // User has pressed button.
+            int arrayIndexAvg1 = RandomNumber(0, alphabet.Length - 1);
+            int arrayIndexAvg2 = RandomNumber(0, alphabet.Length - 1);
+            int arrayIndexAvg3 = RandomNumber(0, alphabet.Length - 1);
+
+            int arrayIndex = (arrayIndexAvg1 + arrayIndexAvg2 + arrayIndexAvg3) / 3;
+
+            RandomVal.Text = Convert.ToString(alphabet[arrayIndex]);
         }
     }
 }

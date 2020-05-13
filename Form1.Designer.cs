97243247e7059d5fc49a9d21274b1b9cd1ed3595
xFuney (Funey) <56867550+xFuney@users.noreply.github.com>
2020-05-13 12:41:48 +0100

    UI design, complete.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.RandomVal = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // RandomVal
+            // 
+            this.RandomVal.AutoSize = true;
+            this.RandomVal.Font = new System.Drawing.Font("Microsoft Sans Serif", 99.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RandomVal.Location = new System.Drawing.Point(81, 9);
+            this.RandomVal.Name = "RandomVal";
+            this.RandomVal.Size = new System.Drawing.Size(155, 152);
+            this.RandomVal.TabIndex = 0;
+            this.RandomVal.Text = "A";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 152);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(280, 67);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Spin the Dice";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(304, 231);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.RandomVal);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Random Letter Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label RandomVal;
+        private System.Windows.Forms.Button button1;
     }
 }
 
